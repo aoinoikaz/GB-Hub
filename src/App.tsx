e946260e2bca64799b7032affa3baaf15b159be6
Dev <devontomlin97@gmail.com>
2025-06-01 12:54:54 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -24,12 +24,7 @@ const App = () => {
       <Route element={<ProtectedRoute />}>
         <Route element={<Layout><Dashboard /></Layout>} path="/dashboard" />
         <Route element={<Layout><MediaDashboard /></Layout>} path="/media" />
-
-
-        {/*  To this (temporarily) */}
-        <Route element={<Store />} path="/store" />
-
-
+        <Route element={<Layout><Store /></Layout>} path="/store" />
         <Route element={<Layout><Settings /></Layout>} path="/settings" />
         <Route element={<Layout><Leaderboard /></Layout>} path="/leaderboard" />
         
